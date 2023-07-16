c666a115a607cd3946a7e0e9b07898fd0941b140
Vladimir Malakovski <vladicabt@hotmail.com>
2023-07-16 20:28:45 +0200

    Display songs and artists using harcoded data

--- src/main/java/com/malvladimir/musicstore/Main.java
+++ src/main/java/com/malvladimir/musicstore/Main.java
@@ -3,5 +3,25 @@ package com.malvladimir.musicstore;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Music store");
+
+        System.out.println("Artists");
+        printArtists();
+
+        System.out.println("Songs");
+        printSongs();
+
     }
+
+    private static void printSongs() {
+        System.out.println("ID    Name");
+        System.out.println("1 Paradise");
+        System.out.println("2 Radioactive");
+    }
+
+    private static void printArtists() {
+        System.out.println("ID    Name");
+        System.out.println("1 Coldplay");
+        System.out.println("2 Imagine dragons");
+    }
+
 }
